2d117c706921d00553927297954e9993806c3e5d
Anton Bouter <bouter@cwi.nl>
2022-11-30 10:56:01 +0100

    main demo file

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ class YourPythonFitnessFunction(gomea.fitness.PythonFitnessFunctionRealValued):
         x = variables[subfunction_index]
         y = variables[subfunction_index+1]
         return 100*(y-x*x)*(y-x*x) + (1.0-x)*(1.0-x)
-    
 
 #frv = gomea.fitness.SphereFunction(10000,value_to_reach=1e-10)
 #frv = gomea.fitness.RosenbrockFunction(20,value_to_reach=1e-10)
 frv = YourPythonFitnessFunction(20,value_to_reach=1e-10)
-lm = gomea.linkage.Univariate()
 #lm = gomea.linkage.MarginalProductModel(1)
-rvgom = gomea.RealValuedGOMEA(fitness=frv,linkage_model=lm,lower_init_range=-115,upper_init_range=-100,random_seed=100) #, maximum_number_of_populations=1, base_population_size=20)
+#lm = gomea.linkage.Univariate()
+#lm = gomea.linkage.LinkageTree(filtered=False,similarity_measure=5,maximum_set_size=10)
+lm = gomea.linkage.UCondHG()
+#lm = gomea.linkage.FromFile("FOS.in")
+#lm = gomea.linkage.Full()
+rvgom = gomea.RealValuedGOMEA(fitness=frv,linkage_model=lm,lower_init_range=-115,upper_init_range=-100,random_seed=100, maximum_number_of_populations=1, base_population_size=40)
 result = rvgom.run()
 
-fd = gomea.fitness.OneMaxFunction(1000)
-dgom = gomea.DiscreteGOMEA(fitness=fd,linkage_model=lm) 
-result = dgom.run()
+#fd = gomea.fitness.OneMaxFunction(1000)
+#dgom = gomea.DiscreteGOMEA(fitness=fd,linkage_model=lm) 
+#result = dgom.run()
