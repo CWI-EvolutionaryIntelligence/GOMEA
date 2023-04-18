9d899137b1f2a4adef91d48f0d2d7d270313b163
Anton Bouter <bouter@cwi.nl>
2023-04-18 13:15:09 +0200

    export symbols script

---
+++ export_symbols.sh
@@ -0,0 +1,17 @@
+str=$(pip show gomea | grep "Location:")
+libdir="${str/Location: /}/"
+echo $libdir
+
+#> symbols.txt
+#> symbols-unmangled.txt
+for f in $libdir/gomea/*.so
+do
+	echo $f #>> symbols.txt
+	echo "Symbols"
+	echo "============================================"
+	nm $f #>> symbols.txt
+	echo $f #>> symbols-unmangled.txt
+	echo "Symbols unmangled"
+	echo "============================================"
+	nm -C $f #>> symbols-unmangled.txt
+done
