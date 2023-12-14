ec739176d96bd34c53204e2d5ac9f0e92e1bea2e
Anton Bouter <Anton.Bouter@cwi.nl>
2023-12-14 12:17:30 +0100

    fix constraint value of python function

--- gomea/src/fitness/bbo_fitness.hpp
+++ gomea/src/fitness/bbo_fitness.hpp
@@ -21,13 +21,13 @@ class BBOFitnessFunction_t : public fitness_t<T>
 
 		double objectiveFunction( int objective_index, solution_t<T> *solution );
 		virtual double objectiveFunction( int objective_index, vec_t<T> &variables ) = 0; 
+		
+		double constraintFunction( solution_t<T> *solution );
+		virtual double constraintFunction( vec_t<T> &variables );
 
 	private:
 		void evaluationFunction( solution_t<T> *solution );
 		void partialEvaluationFunction( solution_t<T> *parent, partial_solution_t<T> *solution );
-		
-		double constraintFunction( solution_t<T> *solution );
-		virtual double constraintFunction( vec_t<T> &variables );
 };
 
 }}
