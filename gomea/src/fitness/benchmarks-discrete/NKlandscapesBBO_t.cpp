695bfb4250de49c68322e112631205928cd3278f
Anton Bouter <Anton.Bouter@cwi.nl>
2024-08-29 16:11:56 +0200

    change subfunctions and make fitness integers

--- gomea/src/fitness/benchmarks-discrete/NKlandscapesBBO_t.cpp
+++ gomea/src/fitness/benchmarks-discrete/NKlandscapesBBO_t.cpp
@@ -22,22 +22,22 @@ void NKlandscapesBBO_t::initializeFitnessTables()
 	static std::uniform_real_distribution<double> distribution(0.0,1.0);
 	std::mt19937 rng(fitness_table_seed);
 
-	fitness_tables.resize( number_of_variables );
-	for( int i = 0; i < number_of_variables; i++ )
+	fitness_tables.resize( number_of_variables - K + 1 );
+	for( int i = 0; i < fitness_tables.size(); i++ )
 	{
 		fitness_tables[i].resize( 1<<K );
 		for( int j = 0; j < (1<<K); j++ )
-			fitness_tables[i][j] = distribution(rng);
+			fitness_tables[i][j] = (int)(1e6*distribution(rng));
 	}
 }
 		
 double NKlandscapesBBO_t::objectiveFunction( int objective_index, vec_t<char> &variables )
 {
 	double fitness = 0;
-	for( int i = 0; i < number_of_variables; i++ )
+	for( int i = 0; i < fitness_tables.size(); i++ )
 	{
 		uint fitness_key = 0; // integer representation of bits that are used as input to the subfunction
-		for( int j = 0; j < K && i+j < number_of_variables; j++ )
+		for( int j = 0; j < K; j++ )
 		{
 			int bit_index = (i + j);
 			fitness_key = (fitness_key<<1) + variables[bit_index];
--- gomea/src/fitness/benchmarks-discrete/NKlandscapes_t.cpp
+++ gomea/src/fitness/benchmarks-discrete/NKlandscapes_t.cpp
@@ -20,24 +20,24 @@ void NKlandscapes_t::initializeFitnessTables()
 	static std::uniform_real_distribution<double> distribution(0.0,1.0);
 	std::mt19937 rng(fitness_table_seed);
 
-	fitness_tables.resize( number_of_variables );
-	for( int i = 0; i < number_of_variables; i++ )
+	fitness_tables.resize( number_of_variables - K + 1 );
+	for( int i = 0; i < fitness_tables.size(); i++ )
 	{
 		fitness_tables[i].resize( 1<<K );
 		for( int j = 0; j < (1<<K); j++ )
-			fitness_tables[i][j] = distribution(rng);
+			fitness_tables[i][j] = (int)(1e6*distribution(rng));
 	}
 }
 
 int NKlandscapes_t::getNumberOfSubfunctions() 
 {
-	return number_of_variables;
+	return number_of_variables - K + 1;
 }
 		
 double NKlandscapes_t::subfunction( int subfunction_index, vec_t<char> &variables )
 {
 	uint fitness_key = 0; // integer representation of bits that are used as input to the subfunction
-	for( int i = 0; (i < K && subfunction_index+i < number_of_variables); i++ )
+	for( int i = 0; i < K; i++ )
 	{
 		int bit_index = (subfunction_index + i);
 		fitness_key = (fitness_key<<1) + variables[bit_index];
@@ -49,7 +49,7 @@ double NKlandscapes_t::subfunction( int subfunction_index, vec_t<char> &variable
 vec_t<int> NKlandscapes_t::inputsToSubfunction( int subfunction_index )
 {
 	vec_t<int> inputs;
-	for( int i = 0; (i < K && subfunction_index+i < number_of_variables); i++ )
+	for( int i = 0; i < K; i++ )
 		inputs.push_back( (subfunction_index + i) );
 	return( inputs );
 }
