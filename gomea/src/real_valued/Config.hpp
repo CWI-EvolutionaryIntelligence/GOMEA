f5f62e1c1c83312495b028a37f58325d34da82d8
Anton Bouter <Anton.Bouter@cwi.nl>
2024-09-02 12:00:04 +0200

    add linker flag and add some initialization

--- gomea/src/real_valued/Config.hpp
+++ gomea/src/real_valued/Config.hpp
@@ -28,7 +28,7 @@ class Config {
 		/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 		/*-=-=-=-=-=-=-=-=-=-=-=- Options -=-=-=-=-=-=-=-=-=-=-=-=-*/
-		int problem_index;
+		int problem_index = -1;
 		bool   use_guidelines,
 			   use_vtr,
 			   print_verbose_overview,                              /* Whether to print a overview of settings (0 = no). */
@@ -40,7 +40,7 @@ class Config {
 			   update_elitist_during_gom,
 			   verbose;
 		int    number_of_variables,									/* The number of variables in the optimization problem. */
-			   FOSIndex = -1,											/* The index of the FOS to be used. */					
+			   FOSIndex = -1,										/* The index of the FOS to be used. */					
 			   base_population_size,                                /* The size of the first population in the multi-start scheme. */
 			   maximum_number_of_populations = 25,                  /* The maximum number of populations in the multi-start scheme. */
 			   maximum_number_of_generations = -1,                  /* The maximum number of generations before any population in the multi-start scheme will be terminated. */
@@ -52,13 +52,13 @@ class Config {
 			   distribution_multiplier_increase,                    /* The multiplicative distribution multiplier increase. */
 			   distribution_multiplier_decrease,                    /* The multiplicative distribution multiplier decrease. */
 			   st_dev_ratio_threshold,                              /* The maximum ratio of the distance of the average improvement to the mean compared to the distance of one standard deviation before triggering AVS (SDR mechanism). */
-			   fitness_variance_tolerance;                          /* The minimum fitness variance level that is allowed. */
-		double vtr,                                           /* The value-to-reach (function value of best solution that is feasible). */
-			   lower_user_range,                              /* The initial lower range-bound indicated by the user (same for all dimensions). */
-			   upper_user_range;                              /* The initial upper range-bound indicated by the user (same for all dimensions). */
+			   fitness_variance_tolerance = 0;                          /* The minimum fitness variance level that is allowed. */
+		double vtr,                                           		/* The value-to-reach (function value of best solution that is feasible). */
+			   lower_user_range = 0.0,                              /* The initial lower range-bound indicated by the user (same for all dimensions). */
+			   upper_user_range = 1.0;                              /* The initial upper range-bound indicated by the user (same for all dimensions). */
 		fitness_pt<double> fitness;
-        linkage_config_t *linkage_config;
-		long long random_seed;
+        linkage_config_t *linkage_config = NULL;
+		long long random_seed = 0;
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 };
 
