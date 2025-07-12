7e6b3d816d1c46d949dcfb8cbd0e42960ba30ce6
Anton Bouter <anton.bouter@cwi.nl>
2025-07-12 14:55:41 +0200

    remove unused embed class

--- gomea/src/real_valued/rv-gomea.hpp
+++ gomea/src/real_valued/rv-gomea.hpp
@@ -5,7 +5,6 @@
 #include "gomea/src/real_valued/tools.hpp"
 #include "gomea/src/real_valued/linkage_model.hpp"
 #include "gomea/src/real_valued/Config.hpp"
-#include "gomea/src/utils/embed.hpp"
 #include "gomea/src/fitness/fitness.hpp"
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
